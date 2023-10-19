2ff9d41d6f13ab8c94f7b2c98482bd17c9efe32f
Jnapika1 <jnapika.aish@gmail.com>
2023-10-19 16:51:44 +0530

    changed models using mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   // console.log(req.user);
-  const product = new Product(title, price, imageUrl, description, null, req.user._id);
+  const product = new Product({
+    title: title, 
+    price: price, 
+    imageUrl: imageUrl, 
+    description: description
+  });
   product.save()
     .then(result => {
       // console.log(result);
@@ -57,8 +62,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, prodId);
-    product.save().then(result => {
+  Product.findById(prodId).then(product=>{
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    return product.save()
+  })
+   .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
@@ -66,7 +77,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -79,7 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
